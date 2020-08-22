a2d37511d534c91be6ae7a0e4d85828823e01e10
Martín Ubaque <soporte@PORTSFORERO.camacol2.local>
2020-08-22 16:34:24 -0500

    Todos las instrucciones hasta 3.4

---
+++ LAB2/test_shellsortmovies.py
@@ -0,0 +1,60 @@
+import pytest
+import config as cf
+from Sorting import shellsort as sort
+from DataStructures import listiterator as it
+from ADT import list as lt
+import csv
+
+#list_type = 'ARRAY_LIST'
+list_type = 'SINGLE_LINKED'
+
+lst_books = lt.newList(list_type)
+booksfile = cf.data_dir + 'GoodReads/books.csv'
+
+def setUp():
+    print('Loading books')
+    loadCSVFile(booksfile, lst_books)
+    print(lst_books['size'])
+
+
+def tearDown():
+       pass
+
+
+def loadCSVFile(file, lst):
+    input_file = csv.DictReader(open(file, encoding = "utf-8"))
+    for row in input_file:
+        lt.addLast(lst, row)
+
+def printList(lst):
+    iterator = it.newIterator(lst)
+    while it.hasNext(iterator):
+        element = it.next(iterator)
+        print(element['goodreads_book_id'])
+
+def less(element1, element2):
+    if int(element1['goodreads_book_id']) < int(element2['goodreads_book_id']):
+        return True
+    return False
+
+def test_sort():
+    """
+    Lista con elementos en orden aleatorio
+    """
+    print("sorting ....")
+    sort.shellSort(lst_books, less)
+
+def test_loading_CSV_y_ordenamiento():
+    """
+    Prueba que se pueda leer el archivo y que despues de relizar el sort, el orden este correcto
+    """
+    setUp()
+    sort.shellSort(lst_books,less)
+    while not (lt.isEmpty(lst_books)):
+        x = int(lt.removeLast(lst_books)['goodreads_book_id'])
+        if not (lt.isEmpty(lst_books)):
+            y = int(lt.lastElement(lst_books)['goodreads_book_id'])
+        else:
+            break
+        assert x > y
+
---
+++ LAB2/test_singlelinkedlistmovies.py
@@ -0,0 +1,31 @@
+import pytest
+import csv
+from DataStructures import listiterator as it
+from DataStructures import liststructure as lt
+
+def test_carga():
+    lista=[]
+    lst=lt.newList()
+
+    file="Data/MoviesCastingRaw-small.csv"
+    sep=";"
+    dialect=csv.excel()
+    dialect.delimiter=sep
+
+    assert (lt.size(lst)==0),"la lista no empieza en 0"
+
+
+    try:
+        with open (file,encoding="utf-8") as csvfile:
+            reader=csv.DictReader(csvfile, dialect=dialect)
+
+            for row in reader:
+                lista.append(row)
+                lt.addLast(lst,row)
+    except: 
+        assert False, "Error"
+
+    assert len(lista)==lt.size(lst), "Son diferentes tamaños"
+    for i in range (len(lista)):
+        assert lista[i]==lt.getElement(lst,i+1)
+
