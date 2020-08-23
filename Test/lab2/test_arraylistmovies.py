6d70921328fec26830a55e43e81ff4b185de4de5
VEC0512 <VEC0512@ad.uniandes.edu.co>
2020-08-23 13:21:45 -0500

    Array List

--- Test/lab2/test_arraylistmovies.py
+++ Test/lab2/test_arraylistmovies.py
@@ -19,3 +19,27 @@ def lst ():
 
 
 
+def loadCSVFile():
+    lista=[]
+    lst = slt.newList()
+
+    moviesfile = 'theMoviesdb/MoviesCastingRaw-small.csv'
+    moviesfile2 = 'theMoviesdb/SmallMoviesDetailsCleaned.csv'
+    sep = ";"
+    dialect = csv.excel()
+    dialect.delimiter=sep
+    assert (slt.size(lst)==0), "La lista no empieza en cero"
+    try:
+        with open(moviesfile, encoding = "utf-8") as csvfile:
+            reader= csv.DictReader(csvfile,dialect=dialect)
+            for row in reader:
+                lista.append(row)
+                slt.addLast(lst, row)
+
+    except:
+        assert False, "Se presento un error al cargar el archivo"
+    assert len(lista) == slt.size(lst), "Son diferentes tamaños"
+
+    for i in range (len(lista)):
+        assert slt.getElement(lst, i*1)== lista[i], "las listas no estan en el mismo orden"
+    
