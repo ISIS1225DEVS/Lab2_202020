a2457fecf10938d37d370184143ac2aabbbbbc87
VEC0512 <VEC0512@ad.uniandes.edu.co>
2020-08-23 13:05:28 -0500

    array list

--- Test/lab2/test_arraylistmovies.py
+++ Test/lab2/test_arraylistmovies.py
@@ -2,6 +2,7 @@ import pytest
 import config 
 import csv
 from DataStructures import arraylist as slt
+from DataStructures import listiterator as it
 
 def cmpfunction (element1, element2):
     if element1["movie_id"] == element2["movie_id"]:
@@ -15,49 +16,30 @@ def cmpfunction (element1, element2):
 def lst ():
     lst = slt.newList(cmpfunction)
     return lst
-#Cargar base de datos
-#lst()
-#lst_movies = slt.newList(cmpfunction)
-moviesfile = config.data_dir + 'theMoviesdb/MoviesCastingRaw-small.csv'
-moviesfile2 = config.data_dir + 'theMoviesdb/SmallMoviesDetailsCleaned.csv'
-#lst = slt.newList(cmpfunction)
-print(type(lst))
 
-def loadCSVFile(file1,file2, lst,sep =";"):
+
+
+def loadCSVFile():
+    lista=[]
+    lst = slt.newList()
+
+    moviesfile = 'theMoviesdb/MoviesCastingRaw-small.csv'
+    moviesfile2 = 'theMoviesdb/SmallMoviesDetailsCleaned.csv'
+    sep = ";"
     dialect = csv.excel()
     dialect.delimiter=sep
-    input_file1 = csv.DictReader(open(file1, encoding = "utf-8"),dialect=dialect)
-    input_file2 = csv.DictReader(open(file2, encoding = "utf-8"),dialect=dialect)
+    assert (slt.size(lst)==0), "La lista no empieza en cero"
+    try:
+        with open(moviesfile, encoding = "utf-8") as csvfile:
+            reader= csv.DictReader(csvfile,dialect=dialect)
+            for row in reader:
+                lista.append(row)
+                slt.addLast(lst, row)
+
+    except:
+        assert False, "Se presento un error al cargar el archivo"
+    assert len(lista) == slt.size(lst), "Son diferentes tamaños"
+
+    for i in range (len(lista)):
+        assert slt.getElement(lst, i*1)== lista[i], "las listas no estan en el mismo orden"
     
-    for row in input_file1:
-        slt.addLast(lst, row)
-    pos=0
-    for row in input_file2: 
-        lst["elements"][pos].update({"Details_claned":row})
-        pos+=1
-  
-
-
-         
-
-#loadCSVFile(moviesfile,moviesfile2,lst)
-
-#print(lst["elements"][1]["Details_claned"]["genres"])
-
-
-
-
-
-
-
-@pytest.fixture
-def books ():
-    books = []
-    books.append({'book_id':'1', 'book_title':'Title 1', 'author':'author 1'})
-    books.append({'book_id':'2', 'book_title':'Title 2', 'author':'author 2'})
-    books.append({'book_id':'3', 'book_title':'Title 3', 'author':'author 3'})
-    books.append({'book_id':'4', 'book_title':'Title 4', 'author':'author 4'})
-    books.append({'book_id':'5', 'book_title':'Title 5', 'author':'author 5'})
-    print (books[0])
-    return books
-
