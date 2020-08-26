528aaae7b241ca37d3067f2c839d667f8b9d6549
Netiel <j.diazi@uniandes.edu.co>
2020-08-26 17:17:29 -0500

    test linked y array

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 import sys
 file_path = os.path.join(os.path.dirname(__file__), '../..')
-os.chdir('..')
-os.chdir('..')
-file_dir = os.getcwd()
+file_dir = os.path.dirname(os.path.realpath('__Proyect__'))
 sys.path.insert(0, os.path.abspath(file_path))
 data_dir = file_dir + '/Data/'
