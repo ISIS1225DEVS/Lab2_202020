a3aacc77a9a044f3b20b018613bc92afe0c85d33
Laura Restrepo <laura@MacBook-Pro-de-Laura-2.local>
2020-08-26 23:17:03 -0500

    hola

--- Test/sorting/less_greater.py
+++ Test/sorting/less_greater.py
@@ -1,8 +1,16 @@
-def less(element1, element2):
-    if int(element1['book_id']) < int(element2['book_id']):
+def less_count(element1, element2):
+    if int(element1['vote_count']) < int(element2['vote_count']):
+        return True
+    return False
+
+def less_average(element1, element2):
+    if float(element1['vote_average']) < float(element2['vote_average']):
         return True
     return False
 
 
-def greater(element1, element2):
-    return not(less(element1, element2))
+def greater_count(element1, element2):
+    return not(less_count(element1, element2))
+
+def greater_average(element1, element2):
+    return not(less_average(element1, element2))
